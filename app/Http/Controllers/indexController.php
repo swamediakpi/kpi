dc0f7f3be2b8c8f99d4f823665db1b8d7de047c5
jonathanyohanes19 <jonathanyohanes19@gmail.com>
2018-08-28 10:26:29 +0700

    indexcontroller

--- app/Http/Controllers/indexController.php
+++ app/Http/Controllers/indexController.php
@@ -53,18 +53,19 @@ class indexController extends Controller {
 		$nameAuth = Auth::user()->EMPLOYEE_ID;
 		$projectname = $request->get('nama');
 		if($roleAuth == '4'){
-			$result =	DB::table('project')->join('employee','project.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
-							->join('project_detail','project.PROJECT_DETAIL_ID','=','project_detail.PROJECT_DETAIL_ID')
+			
+			$result = DB::table('project_employee')->join('employee','project_employee.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
+							->join('project','project_employee.PROJECT_DETAIL_ID','=','project.PROJECT_DETAIL_ID')
 							->select('EMPLOYEE_NAME',DB::raw('SUM(WORK_DURATION) AS WORK_DURATION'),'PROJECT_DURATION')
-							->where('project_detail.PROJECT_DETAIL_ID','=',$projectname)
+							->where('project_employee.PROJECT_DETAIL_ID','=',$projectname)
 							->where('employee.EMPLOYEE_ID','=',$nameAuth)
 							->groupBy('EMPLOYEE_NAME')
 							->get();
 		}else{
-			$result =	DB::table('project')->join('employee','project.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
-							->join('project_detail','project.PROJECT_DETAIL_ID','=','project_detail.PROJECT_DETAIL_ID')  
+			$result =	DB::table('project_employee')->join('employee','project_employee.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
+							->join('project','project_employee.PROJECT_DETAIL_ID','=','project.PROJECT_DETAIL_ID')  
 							->select('EMPLOYEE_NAME', DB::raw('SUM(WORK_DURATION) AS WORK_DURATION'),'PROJECT_DURATION')
-							->where('project_detail.PROJECT_DETAIL_ID','=',$projectname)
+							->where('project.PROJECT_DETAIL_ID','=',$projectname)
 							->groupBy('EMPLOYEE_NAME')
 							->get();
 		}
@@ -78,16 +79,16 @@ class indexController extends Controller {
 		$pr_tahun = $request->get('p_tahun');
 		if($request -> ajax()){
 			if($roleAuth == '4'){
-				$temp =	DB::table('project_detail')->select('PROJECT_NAME','PROJECT_DURATION','WORK_DURATION',DB::raw('IFNULL( REALIZE_TIME, 0 ) as realize_time') )
-							->join('project','project_detail.PROJECT_DETAIL_ID','=','project.PROJECT_DETAIL_ID')
-							->join('employee','project.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
+				$temp =	DB::table('project')->select('PROJECT_NAME','PROJECT_DURATION','WORK_DURATION',DB::raw('IFNULL( REALIZE_TIME, 0 ) as realize_time') )
+							->join('project','project_detail.PROJECT_DETAIL_ID','=','project_employee.PROJECT_DETAIL_ID')
+							->join('employee','project_employee.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
 							->where('employee.EMPLOYEE_NAME','=',$nameAuth)
 							->whereYear('PROJECT_START','=',$pr_tahun)
 							->groupBy('PROJECT_NAME')
 							->get();
 			}else{
-				$temp =	DB::table('project_detail')
-							->join('project','project_detail.PROJECT_DETAIL_ID','=','project.PROJECT_DETAIL_ID')
+				$temp =	DB::table('project')
+							->join('project_employee','project.PROJECT_DETAIL_ID','=','project_employee.PROJECT_DETAIL_ID')
 							->select('PROJECT_NAME','PROJECT_DURATION', DB::raw('AVG(WORK_DURATION) AS WORK_DURATION'),DB::raw('IFNULL( REALIZE_TIME, 0 ) as realize_time'))
 							->whereYear('PROJECT_START','=',$pr_tahun)
 							->groupBy('PROJECT_NAME','PROJECT_DURATION')
@@ -104,16 +105,16 @@ class indexController extends Controller {
 		$pr_tahun = $request->get('p_tahun');
 		if($request -> ajax()){
 			if($roleAuth == '4'){
-				$temp = DB::table('project_detail')->select('PROJECT_NAME','PROJECT_DURATION')
-							->join('project','project_detail.PROJECT_DETAIL_ID','=','project.PROJECT_DETAIL_ID')
-							->join('employee','project.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
+				$temp = DB::table('project')->select('PROJECT_NAME','PROJECT_DURATION')
+							->join('project_employee','project.PROJECT_DETAIL_ID','=','project_employee.PROJECT_DETAIL_ID')
+							->join('employee','project_employee.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
 							->where('employee.EMPLOYEE_NAME','=',$nameAuth)
 							->whereYear('PROJECT_START','=',$pr_tahun)
 							->groupBy('PROJECT_NAME')
 							->get();
 			}else{
-				$temp = DB::table('project_detail')->select('PROJECT_NAME','PROJECT_DURATION')
-							->whereYear('PROJECT_START','=',$pr_tahun)   
+				$temp = DB::table('project')->select('PROJECT_NAME','PROJECT_DURATION')
+							->whereYear('PROJECT_START','LIKE',$pr_tahun)   
 							->get();
 			}
 			$x = json_encode($temp);
@@ -126,15 +127,15 @@ class indexController extends Controller {
 		$nameAuth = Auth::user()->EMPLOYEE_NAME;
 		$pr_tahun = $request->get('p_tahun');
 		if($roleAuth == '4'){
-			$projectname =	DB::table('project_detail')->select('PROJECT_NAME','project_detail.PROJECT_DETAIL_ID')
-								->join('project','project_detail.PROJECT_DETAIL_ID','=','project.PROJECT_DETAIL_ID')
-								->join('employee','project.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
+			$projectname =	DB::table('project')->select('PROJECT_NAME','project_detail.PROJECT_DETAIL_ID')
+								->join('project_employee','project.PROJECT_DETAIL_ID','=','project_employee.PROJECT_DETAIL_ID')
+								->join('employee','project_employee.EMPLOYEE_ID','=','employee.EMPLOYEE_ID')
 								->where('employee.EMPLOYEE_NAME','=',$nameAuth)
 								->whereYear('PROJECT_START','=',$pr_tahun)
 								->groupBy('PROJECT_NAME')
 								->get();
 		}else{
-			$projectname =	DB::table('project_detail')->select('PROJECT_NAME','PROJECT_DETAIL_ID')
+			$projectname =	DB::table('project')->select('PROJECT_NAME','PROJECT_DETAIL_ID')
 								->whereYear('PROJECT_START','=',$pr_tahun)
 								->orderBy('PROJECT_NAME','asc')
 								->get();
