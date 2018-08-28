9854ce5b7ca3742fd373a4864fc2534b5306ed38
Deden <dedensmkn4@gmail.com>
2018-08-28 17:05:00 +0700

    migrate

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '172.17.3.11'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'petamudi_kpi'),
+            'username' => env('DB_USERNAME', 'kpibim'),
+            'password' => env('DB_PASSWORD', 'kpi123'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
